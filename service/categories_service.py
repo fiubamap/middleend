1803a9f6a5d2621a2688430b0706c18aa0feca03
Josefina Montenegro <jmontenegro@fi.uba.ar>
2023-09-09 15:51:47 -0300

    WIP fix

--- service/categories_service.py
+++ service/categories_service.py
@@ -1,4 +1,6 @@
 import json
+from json import JSONDecodeError
+
 import requests
 import itertools
 from flask import make_response
@@ -7,7 +9,8 @@ from settings import GEOSERVER_BASE_URL, GEOSERVER_PASSWORD, GEOSERVER_USERNAME
 
 def build_categories():
     workspaces_body = get(GEOSERVER_BASE_URL + '/workspaces')
-    categories = list(filter(None, map(lambda workspace: build_category(workspace), workspaces_body['workspaces']['workspace'])))
+    categories = list(
+        filter(None, map(lambda workspace: build_category(workspace), workspaces_body['workspaces']['workspace'])))
     return make_response(json.dumps(categories), 200)
 
 
@@ -29,7 +32,9 @@ def build_subcategories_from_data_stores(workspace):
     body = get(GEOSERVER_BASE_URL + '/workspaces/' + workspace['name'] + '/datastores')
     if body['dataStores'] != '':
         data_stores = body['dataStores']['dataStore']
-        subcategories = list(filter(None, map(lambda data_store: build_subcategory_from_data_store(data_store, workspace['name']), data_stores)))
+        subcategories = list(filter(None, map(lambda data_store: build_subcategory_from_data_store(data_store,
+                                                                                                   workspace['name']),
+                                              data_stores)))
         return subcategories
     print("No dataStores found for category: " + workspace['name'])
     return []
@@ -39,7 +44,10 @@ def build_subcategories_from_coverage_stores(workspace):
     body = get(GEOSERVER_BASE_URL + '/workspaces/' + workspace['name'] + '/coveragestores')
     if body['coverageStores'] != '':
         coverage_stores = body['coverageStores']['coverageStore']
-        subcategories = list(filter(None, map(lambda coverage_store: build_subcategory_from_coverage_store(coverage_store, workspace['name']), coverage_stores)))
+        subcategories = list(filter(None,
+                                    map(lambda coverage_store: build_subcategory_from_coverage_store(coverage_store,
+                                                                                                     workspace['name']),
+                                        coverage_stores)))
         if len(subcategories) != 0 and subcategories != [{}]:
             return subcategories
     print("No coverageStores found for category: " + workspace['name'])
@@ -50,7 +58,9 @@ def build_subcategories_from_wms_stores(workspace):
     body = get(GEOSERVER_BASE_URL + '/workspaces/' + workspace['name'] + '/wmsstores')
     if body['wmsStores'] != '':
         wms_stores = body['wmsStores']['wmsStore']
-        subcategories = list(filter(None, map(lambda wms_store: build_subcategory_from_wms_store(wms_store, workspace['name']), wms_stores)))
+        subcategories = list(filter(None, map(lambda wms_store: build_subcategory_from_wms_store(wms_store,
+                                                                                                 workspace['name']),
+                                              wms_stores)))
         if len(subcategories) != 0 and subcategories != [{}]:
             return subcategories
     print("No wmsStores found for category: " + workspace['name'])
@@ -61,7 +71,9 @@ def build_subcategories_from_wmts_stores(workspace):
     body = get(GEOSERVER_BASE_URL + '/workspaces/' + workspace['name'] + '/wmtsstores')
     if body['wmtsStores'] != '':
         data_stores = body['wmtsStores']['wmtsStore']
-        subcategories = list(filter(None, map(lambda data_store: build_subcategory_from_wmts_store(data_store, workspace['name']), data_stores)))
+        subcategories = list(filter(None, map(lambda data_store: build_subcategory_from_wmts_store(data_store,
+                                                                                                   workspace['name']),
+                                              data_stores)))
         if len(subcategories) != 0 and subcategories != [{}]:
             return subcategories
     print("No wmtsStores found for category: " + workspace['name'])
@@ -73,7 +85,8 @@ def build_subcategory_from_data_store(data_store, workspace_name):
     body = get(feature_types_href)
     if body != '' and body['featureTypes'] != '':
         data_layers = body['featureTypes']['featureType']
-        layers = list(filter(None, map(lambda data_layer: build_layer_from_data_store(data_layer, workspace_name), data_layers)))
+        layers = list(
+            filter(None, map(lambda data_layer: build_layer_from_data_store(data_layer, workspace_name), data_layers)))
         return build_subcategory(data_store['name'], layers)
     print('No layers found for category: ' + workspace_name + ' and subcategory: ' + data_store['name'])
     return
@@ -84,7 +97,9 @@ def build_subcategory_from_coverage_store(coverage_store, workspace_name):
     body = get(coverage_layers_href)
     if body != '' and body['coverages'] != '':
         coverage_layers = body['coverages']['coverage']
-        layers = list(filter(None, map(lambda coverage_layer: build_layer_from_coverage_store(coverage_layer, workspace_name), coverage_layers)))
+        layers = list(filter(None,
+                             map(lambda coverage_layer: build_layer_from_coverage_store(coverage_layer, workspace_name),
+                                 coverage_layers)))
         return build_subcategory(coverage_store['name'], layers)
     print('No layers found for category: ' + workspace_name + ' and subcategory: ' + coverage_store['name'])
     return
@@ -95,7 +110,8 @@ def build_subcategory_from_wms_store(wms_store, workspace_name):
     body = get(wms_layers_href)
     if body != '' and body['wmsLayers'] != '':
         wms_layers = body['wmsLayers']['wmsLayer']
-        layers = list(filter(None, map(lambda wms_layer: build_layer_from_wms_store(wms_layer, workspace_name), wms_layers)))
+        layers = list(
+            filter(None, map(lambda wms_layer: build_layer_from_wms_store(wms_layer, workspace_name), wms_layers)))
         return build_subcategory(wms_store['name'], layers)
     print('No layers found for category: ' + workspace_name + ' and subcategory: ' + wms_store['name'])
     return
@@ -106,7 +122,8 @@ def build_subcategory_from_wmts_store(wmts_store, workspace_name):
     body = get(wmts_layers_href)
     if body != '' and body['wmtsLayers'] != '':
         wmts_layers = body['wmtsLayers']['wmtsLayer']
-        layers = list(filter(None, map(lambda wmts_layer: build_layer_from_wmts_store(wmts_layer, workspace_name), wmts_layers)))
+        layers = list(
+            filter(None, map(lambda wmts_layer: build_layer_from_wmts_store(wmts_layer, workspace_name), wmts_layers)))
         return build_subcategory(wmts_store['name'], layers)
     print('No layers found for category: ' + workspace_name + ' and subcategory: ' + wmts_store['name'])
     return
@@ -114,7 +131,10 @@ def build_subcategory_from_wmts_store(wmts_store, workspace_name):
 
 def get(url):
     response = requests.get(url, auth=(GEOSERVER_USERNAME, GEOSERVER_PASSWORD))
-    return json.loads(response.content)
+    try:
+        return json.loads(response.content)
+    except JSONDecodeError:
+        return {}
 
 
 def build_subcategory(store_name, layers):
@@ -124,13 +144,15 @@ def build_subcategory(store_name, layers):
 
 
 def build_layer_from_data_store(layer, workspace_name):
-    feature_type = get(layer['href'])['featureType']
-    if feature_type['enabled'] and (feature_type.get('advertised') is not None and feature_type['advertised']):
-        response = {'name': workspace_name + ':' + feature_type['name'], 'title': feature_type['name']}
-        if feature_type.get('abstract') is not None:
-            response['description'] = feature_type['abstract']
-        return response
-    return
+    res = get(layer['href'])
+    if hasattr(res, 'featureType'):
+        feature_type = res['featureType']
+        if feature_type['enabled'] and (feature_type.get('advertised') is not None and feature_type['advertised']):
+            response = {'name': workspace_name + ':' + feature_type['name'], 'title': feature_type['name']}
+            if feature_type.get('abstract') is not None:
+                response['description'] = feature_type['abstract']
+            return response
+        return
 
 
 def build_layer_from_coverage_store(layer, workspace_name):
